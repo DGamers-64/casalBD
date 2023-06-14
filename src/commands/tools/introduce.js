71603b220e4d03d7e3c66cd5517101b151b79d5f
DGamers-64 <dgamers.643@gmail.com>
2023-06-14 22:59:58 +0200

    Implementado mensaje con id automatico

--- src/commands/tools/introduce.js
+++ src/commands/tools/introduce.js
@@ -1,15 +1,12 @@
 const Items = require('../../schemas/items')
 const { SlashCommandBuilder } = require('discord.js');
 const mongoose = require('mongoose');
+let itemId = 0;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('introduce')
         .setDescription('Introduce varias imagenes a la base de datos')
-        .addStringOption(option =>
-            option.setName('id')
-            .setDescription('id del item')
-            .setRequired(true))
         .addStringOption(option =>
             option.setName('image1')
             .setDescription('primera imagen')
@@ -24,25 +21,24 @@ module.exports = {
     async execute(interaction, client) {
         let ItemsProfile = await Items;
         ItemsProfile = await new Items({
-            itemId: interaction.options.getString(ItemsProfile.itemId),
-            image1: interaction.options.getString(ItemsProfile.image1),
-            image2: interaction.options.getString(ItemsProfile.image2),
-            image3: interaction.options.getString(ItemsProfile.image3),
+            itemId: itemId,
+            image1: interaction.options.getString('image1'),
+            image2: interaction.options.getString('image2'),
+            image3: interaction.options.getString('image3'),
         });
+
+        itemId=itemId+1
 
         await ItemsProfile.save().catch(console.error);
         console.log("Nuevo registro!");
 
-        if (interaction.options.getString(ItemsProfile.image3)=="") {
+        if (!interaction.options.getString('image3')) {
             await interaction.reply({
-                content: `Imagen 1: ${ItemsProfile.imagen1}`,
-                content: `Imagen 2: ${ItemsProfile.imagen2}`
+                content: `Imagen 1: ${ItemsProfile.image1}\nImagen 2: ${ItemsProfile.image2}`
             });
         } else {
             await interaction.reply({
-                content: `Imagen 1: ${ItemsProfile.imagen1}`,
-                content: `Imagen 2: ${ItemsProfile.imagen2}`,
-                content: `Imagen 3: ${ItemsProfile.imagen3}`
+                content: `Imagen 1: ${ItemsProfile.image1}\nImagen 2: ${ItemsProfile.image2}\nImagen 3: ${ItemsProfile.image3}`
             });                
         } 
     },
