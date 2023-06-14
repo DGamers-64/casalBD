5d168d5c5963097ea74a28e0f9cdf0443a5286a8
DGamers-64 <dgamers.643@gmail.com>
2023-06-14 05:51:00 +0200

    Primeras Configuraciones

---
+++ src/bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { token } = process.env;
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+
+const client = Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection();
+client.commandsArray = [];
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+    const functionFiles = fs
+    .readdirSync(`.src/function/${folder}`)
+    .filter(file => file.endsWith('.js'));
+    for (const file of functionFiles) require(`./functions/${folder}/${file}`)(client);
+}
+
