2077140dbe08bcca30667ccc268d0eaa74e6a5c2
DGamers-64 <dgamers.643@gmail.com>
2023-06-17 09:32:33 +0200

    colores fix

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -4,6 +4,6 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        console.log(chalk.green(`${client.user.tag} ha sido logueado y se encuentra online.`));
+        console.log(chalk.yellow(`${client.user.tag} ha sido logueado y se encuentra online.`));
     }
 }
